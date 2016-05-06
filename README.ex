79184cea6e7610cf99aabd0d6cfee0fd9e5e9a81
Julen Larrucea <julenl@gmail.com>
2016-05-06 14:38:55 +0200

    Basic charm and tests for rabbitmq

---
+++ README.ex
@@ -0,0 +1,17 @@
+# Overview
+
+This is a test charm to install and test rabbitmq.
+
+# Usage
+
+juju deploy rabbitmq-single
+
+# Configuration
+
+There is no configuration. It runs everything by default.
+
+# Contact Information
+
+Julen Larrucea: julen@larrucea.eu
+IRC: julen / julenl
+
